b780eb441844dd9ee79d3252e865fbd1121f39fe
gzamfir-ca <gzamfir_ca@icloud.com>
2026-04-30 12:58:12 -0400

    + main code

--- lib/markdown/jotter.rb
+++ lib/markdown/jotter.rb
@@ -1,10 +1,110 @@
 # frozen_string_literal: true
 
+require "thor"
 require_relative "jotter/version"
 
+# Provides quick note creation abilities
 module Markdown
+  # Provides jotter implementation
   module Jotter
-    class Error < StandardError; end
-    # Your code goes here...
+    BASE_COLOR = "\e[0m"
+    PASS_COLOR = "\e[1;32m"
+
+    # Provides CLI implementation
+    class CLI < Thor
+      # Class public methods
+      desc "help [COMMAND]", "Describes cli command"
+
+      def help(command = nil)
+        super
+      end
+
+      desc "compose NOTE", "Creates a new MD note"
+      option :path, type: :string, aliases: "-p", desc: "Path to note location"
+
+      def compose(note)
+        exit Jotter.compose(note, options[:path])
+      end
+
+      desc "tree", "Tree for all commands"
+
+      def tree
+        build_command_tree(self.class, "")
+      end
+
+      desc "version", "Prints version number"
+
+      def version
+        exit Jotter.version
+      end
+
+      no_commands do
+        def self.exit_on_failure?
+          true
+        end
+
+        def self.start(given_args = ARGV, config = {})
+          Jotter.setup
+          super
+        end
+      end
+    end
+
+    # Module public methods
+    def self.compose(note, root)
+      @root = Pathname.new(root).expand_path if root
+      mark = note_name(note)
+      path = path_name(mark)
+      log_message(__method__, "adding new entry to #{path}")
+      res = (File.exist?(path) || update_file?(path, "w", title_text(mark))) &&
+            update_file?(path, "a", entry_text)
+      res ? 0 : 1
+    end
+
+    def self.setup
+      @root = Pathname.new("~/Desktop").expand_path
+    end
+
+    def self.version
+      puts "#{PASS_COLOR}version: #{Jotter::VERSION}#{BASE_COLOR}"
+      0
+    end
+
+    # Module private methods
+    def self.log_exception(method_name, message, exp)
+      puts "#{name}:#{method_name} #{message} #{exp.message}"
+    end
+
+    def self.log_message(method_name, message)
+      puts "#{name}:#{method_name} #{message}"
+    end
+
+    def self.note_name(note)
+      File.basename(note, ".*").sub(/^\d{8}_/, "").gsub(/[^0-9A-Za-z.-]/, "-")
+    end
+
+    def self.path_name(mark)
+      path = @root.join("#{Time.now.strftime("%Y%m%d")}_#{mark.downcase(:ascii)}.md")
+      path.tap { |p| p.dirname.mkpath }
+    end
+
+    def self.entry_text
+      "\n<<*replace this*>>\n"
+    end
+
+    def self.title_text(mark)
+      formatted_mark = mark.split("-").map(&:capitalize).join(" ")
+      "# #{Time.now.strftime("%Y%m%d")} #{formatted_mark}\n\n #inbox\n"
+    end
+
+    def self.update_file?(file, mode, data)
+      file.open(mode) { |f| f << data }
+      true
+    rescue StandardError => e
+      log_exception(__method__, "modifying #{file} failed: ", e)
+      false
+    end
+
+    private_class_method(:log_exception, :log_message, :note_name, :path_name, :entry_text, :title_text, :update_file?)
   end
 end
